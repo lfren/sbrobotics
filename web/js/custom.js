23269c267bee4933cc6280bd16610154ee7950f7
lfren <jessica.donetski@gmail.com>
2016-08-25 17:03:30 -0400

    maksim base commit

---
+++ web/js/custom.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rebel_000 on 8/24/2016.
+ */
+
+
+$(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+            $(this).toggleClass('open');
+            $('b', this).toggleClass("caret caret-up");
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+            $(this).toggleClass('open');
+            $('b', this).toggleClass("caret caret-up");
+        });
+});
+
